d724d45d999a6aca7a84b07770dae4f6a76effed
siddhant2002 <siddhantswarupmallick@gmail.com>
2022-05-15 12:23:42 +0530

    Number 11 updated

--- Assignment - 1/Number11.java
+++ Assignment - 1/Number11.java
@@ -4,35 +4,33 @@ public class Number11{
      public static void main(String []args){
          Scanner sc=new Scanner(System.in);
          int n=sc.nextInt();
-         int a[]=new int[n];
+         List<Integer> nm=new ArrayList<>();
          for(int i=0;i<n;i++)
          {
-             a[i]=sc.nextInt();
+             nm.add(sc.nextInt());
          }
          System.out.println("The permutations are ");
-         task(a,0,n,n);
+         task(nm,0,n,n);
      }
-     static void task(int a[], int i, int j, int n)
+     static void task(List<Integer> nm, int i, int j, int n)
      {
          if(i==n)
          {
-             for(int p=0;p<n;p++)
-             {
-                 System.out.print(a[p]+" ");
-             }
-             System.out.println();
+             System.out.println(nm);
          }
          for(int p=i;p<j;p++)
          {
-             swap(a,i,p);
-             task(a,i+1,j,n);
-             swap(a,i,p);
+             swap(nm,i,p);
+             task(nm,i+1,j,n);
+             swap(nm,i,p);
          }
      }
-     static void swap(int a[], int i, int j)
+     static void swap(List<Integer> nm, int i, int j)
      {
-         int d=a[i];
-         a[i]=a[j];
-         a[j]=d;
+         int d=nm.get(i);
+         nm.add(i,nm.get(j));
+         nm.remove(i+1);
+         nm.add(j,d);
+         nm.remove(j+1);
      }
 }
