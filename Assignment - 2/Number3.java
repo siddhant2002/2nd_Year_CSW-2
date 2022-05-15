a9cc7731d73e1c8a6b5953e791b18da2f1b215df
siddhant2002 <siddhantswarupmallick@gmail.com>
2022-05-15 20:27:34 +0530

    Number 3 updated

--- Assignment - 2/Number3.java
+++ Assignment - 2/Number3.java
@@ -1,40 +1,41 @@
 import java.util.*;
 public class Number3{
 
-     Scanner sc=new Scanner(System.in);
-         String n=sc.next();
-         int k=n.length();
-         Stack<Integer> nm=new Stack<>();
-         for(int i=0;i<k;i++)
-         {
-             char p=n.charAt(i);
-             if(p=='+'||p=='-'||p=='x'||p=='/')
-             {
-                 int b=nm.pop();
-                 int a=nm.pop();
-                 if(p=='+')
-                 {
-                     a+=b;
-                 }
-                 else if(p=='-')
-                 {
-                     a-=b;
-                 }
-                 else if(p=='x')
-                 {
-                     a*=b;
-                 }
-                 else if(p=='/')
-                 {
-                     a/=b;
-                 }
-                 nm.push(a);
-             }
-             else
-             {
-                 nm.push(n.charAt(i)-'0');
-             }
-         }
-         System.out.println(nm.pop());
-     }
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        String n=sc.next();
+        int k=n.length();
+        Stack<Integer> nm=new Stack<>();
+        for(int i=0;i<k;i++)
+        {
+            char p=n.charAt(i);
+            if(p=='+'||p=='-'||p=='x'||p=='/')
+            {
+                int b=nm.pop();
+                int a=nm.pop();
+                if(p=='+')
+                {
+                    a+=b;
+                }
+                else if(p=='-')
+                {
+                    a-=b;
+                }
+                else if(p=='x')
+                {
+                    a*=b;
+                }
+                else if(p=='/')
+                {
+                    a/=b;
+                }
+                nm.push(a);
+            }
+            else
+            {
+                nm.push(n.charAt(i)-'0');
+            }
+        }
+        System.out.println(nm.pop());
+    }
 }
