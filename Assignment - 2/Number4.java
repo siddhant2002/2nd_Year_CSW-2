ceb762300d2d1a8c4cb7464310ad102be4374bf1
siddhant2002 <siddhantswarupmallick@gmail.com>
2022-05-15 20:50:26 +0530

    Number 3 updated

--- Assignment - 2/Number4.java
+++ Assignment - 2/Number4.java
@@ -1,55 +1,41 @@
 import java.util.*;
 public class Number4{
 
-     public static void main(String []args){
-         Scanner sc=new Scanner(System.in);
-         List<List<Integer>> nm=new ArrayList<List<Integer>>();
-         int n=sc.nextInt();
-         for(int i=0;i<n;i++)
-         {
-             nm.add(new ArrayList<>());
-         }
-         nm.get(0).add(1);
-         nm.get(1).add(2);
-         nm.get(2).add(3);
-         nm.get(3).add(4);
-         nm.get(4).add(0);
-         nm.get(1).add(0);
-         nm.get(2).add(1);
-         nm.get(3).add(2);
-         nm.get(4).add(3);
-         nm.get(5).add(7);
-         nm.get(6).add(3);
-         nm.get(7).add(1);
-         nm.get(8).add(1);
-         nm.get(9).add(1);
-         nm.get(7).add(5);
-         nm.get(3).add(6);
-         nm.get(9).add(1);
-         nm.get(1).add(9);
-         nm.get(2).add(8);
-         nm.get(8).add(2);
-         boolean k[]=new boolean[n];
-         List<Integer> kk=new ArrayList<>();
-         for(int i=0;i<n;i++)
-         {
-             if(k[i]==false)
-             {
-                 task(nm,i,k,kk);
-             }
-         }
-         System.out.println(kk);
-     }
-     static void task(List<List<Integer>> nm, int s, boolean k[], List<Integer> kk)
-     {
-         k[s]=true;
-         kk.add(s);
-         for(int p:nm.get(s))
-         {
-             if(!k[p])
-             {
-                 task(nm,p,k,kk);
-             }
-         }
-     }
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        String n=sc.next();
+        int k=n.length();
+        Stack<Integer> nm=new Stack<>();
+        for(int i=0;i<k;i++)
+        {
+            char p=n.charAt(i);
+            if(p=='+'||p=='-'||p=='x'||p=='/')
+            {
+                int b=nm.pop();
+                int a=nm.pop();
+                if(p=='+')
+                {
+                    a+=b;
+                }
+                else if(p=='-')
+                {
+                    a-=b;
+                }
+                else if(p=='x')
+                {
+                    a*=b;
+                }
+                else if(p=='/')
+                {
+                    a/=b;
+                }
+                nm.push(a);
+            }
+            else
+            {
+                nm.push(n.charAt(i)-'0');
+            }
+        }
+        System.out.println(nm.pop());
+    }
 }
