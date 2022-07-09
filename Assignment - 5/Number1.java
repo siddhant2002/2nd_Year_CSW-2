e4488365a63d6a32dc8ead7e4e73653ffc11908b
siddhant2002 <siddhantswarupmallick@gmail.com>
2022-07-10 02:25:31 +0530

    Number 1 updated

---
+++ Assignment - 5/Number1.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Number1
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		int c=0,m=0;
+		for(int i=0;i<n;i++)
+		{
+		    a[i]=sc.nextInt();
+		    if(a[i]==0)
+		    {
+		        c++;
+		    }
+		    else if(a[i]==1)
+		    {
+		        m++;
+		    }
+		}
+		for(int i=0;i<n;i++)
+		{
+		    if(c!=0)
+		    {
+		        a[i]=0;
+		        c--;
+		    }
+		    else 
+		    {
+		        a[i]=1;
+		        m--;
+		    }
+		}
+		for(int i:a)
+		{
+		    System.out.print(i+" ");
+		}
+	}
+}
