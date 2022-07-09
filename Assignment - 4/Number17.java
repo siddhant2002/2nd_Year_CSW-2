8536516f38ac78e6f2371a5b9e3a40ea5e9454a4
siddhant2002 <siddhantswarupmallick@gmail.com>
2022-07-10 02:18:00 +0530

    Number 17 updated

---
+++ Assignment - 4/Number17.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Number17
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+		    a[i]=sc.nextInt();
+		}
+		Arrays.sort(a);
+        System.out.println("The the n/2th index element is "+a[n/2]);
+	}
+}
