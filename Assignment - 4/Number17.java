f5714c11d00063215814c9ebf23c7953c8e73e53
siddhant2002 <siddhantswarupmallick@gmail.com>
2022-07-10 11:38:14 +0530

    Number 17 updated

--- Assignment - 4/Number17.java
+++ Assignment - 4/Number17.java
@@ -1,15 +1,27 @@
 import java.util.*;
-public class Number17
+class Number17
 {
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int a[]=new int[n];
-		for(int i=0;i<n;i++)
-		{
-		    a[i]=sc.nextInt();
-		}
-		Arrays.sort(a);
-        System.out.println("The the n/2th index element is "+a[n/2]);
-	}
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for (int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int m=Integer.MIN_VALUE,p=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]>m)
+            {
+                m=a[i];
+            }
+            if(a[i]>p&&a[i]!=m)
+            {
+                p=a[i];
+            }
+        }
+        System.out.println("The 2nd largest element is "+p);
+    }
 }
