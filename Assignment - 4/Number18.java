9f05c5d2a924b3d0c27cc0752fbe920be0260786
siddhant2002 <siddhantswarupmallick@gmail.com>
2022-07-10 11:37:23 +0530

    Number 18 updated

--- Assignment - 4/Number18.java
+++ Assignment - 4/Number18.java
@@ -4,18 +4,12 @@ public class Number18
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		List<Integer> nm=new ArrayList<>();
+		int a[]=new int[n];
 		for(int i=0;i<n;i++)
 		{
-		    nm.add(sc.nextInt());
+		    a[i]=sc.nextInt();
 		}
-		for(int i=0;i<nm.size()-1;i++)
-		{
-		    if(nm.get(i)>nm.get(i+1))
-		    {
-		        System.out.println(nm.get(i));
-		        return;
-		    }
-		}
+		Arrays.sort(a);
+        System.out.println("The the n/2th index element is "+a[n/2]);
 	}
 }
